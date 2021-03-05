e75d8544f53b542c62d2f43103a806ec675bf640
Dax246 <damien.ambegoda@gmail.com>
2021-03-05 22:53:12 +1100

    continued to edit main.py

--- flask-api/main.py
+++ flask-api/main.py
@@ -4,22 +4,53 @@ Main file that will contain all the python functions to be called by the front e
 """
 import json
 
-def outfit_selector(weather: str, formality:str):
+def outfit_selector(weather: list, formality:str) -> list:
     """
     Randomly picks outfits that match the weather and formality
 
     :input:
-        weather = string that gives us the summary of the weather (e.g. cold, warm)
+        weather = list of strings. First string is weather, second string (or null) will say rain if it is raining
         formality = string that says formality type (e.g. casual, smart-casual)
+        rain = boolean to tell if going to rain or not
     :return: list of strings that is "colour type" of clothing picked in the outfit
     """
 
+    # Loads list that contains dictionaries from stored clothing
     with open("test2.json") as json_file:
         data = json.load(json_file)
-        # Example showing how to print all the type and colour from every item of clothing
-        for i in data["clothes"]:
-            print(i)
 
+    # List that contains all the cloth
+    selected_clothes = []
+    for i in range(len(data["clothes"])):
+        # matches weather and formality
+        if weather[0] in data["clothes"][i]["condition"] and formality in data["clothes"][i]["formality"]:
+            # checks if clothes viable for rain
+            if len(weather) == 2:
+                if "rain" in data["clothes"][i]["condition"]:
+                    selected_clothes.append(i)
+            else:
+                selected_clothes.append(i)
 
+    top = []
+    middlewear = []
+    outerwear = []
+    bottom = []
+    footwear = []
+    headwear = []
+    for num in selected_clothes:
+        if data["clothes"][num]["category"] == "top":
+            top.append(num)
+        elif data["clothes"][num]["category"] == "middlewear":
+            middlewear.append(num)
+        elif data["clothes"][num]["category"] == "outerwear":
+            outerwear.append(num)
+        elif data["clothes"][num]["category"] == "bottom":
+            bottom.append(num)
+        elif data["clothes"][num]["category"] == "footwear":
+            footwear.append(num)
+        elif data["clothes"][num]["category"] == "headwear":
+            headwear.append(num)
+        print(data["clothes"][num])
 
-outfit_selector("a", "b")
+#output strings
+outfit_selector(["warm"], "casual")
