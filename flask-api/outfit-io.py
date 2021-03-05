b381c8dfcdeba2402c19dcfccbfaf1393f18b9bb
AnCantaloupe <lisuruapeiris@gmail.com>
2021-03-05 22:50:34 +1100

    add filtering clothing

---
+++ flask-api/outfit-io.py
@@ -0,0 +1,9 @@
+import json
+
+def append_new_clothing(string):
+    # @Incomplete
+
+
+def delete_existing_clothing(index):
+    # @Incomplete
+
--- flask-api/weather-outfit logic.py
+++ flask-api/weather-outfit logic.py
@@ -22,17 +22,33 @@ def warmth_factor(temp):
     else:
         return "warm"
 
-print(warmth_factor(weather))
+
+def apply_condition(choices, condition):
+    narrowed_choices = []
+    for i in choices:
+        if condition in i["condition"]:
+            narrowed_choices.append(i)
+    return narrowed_choices
 
 
-def pick():
-    choices = []
-    return choices
+def apply_formality(choices, formality):
+    narrowed_choices = []
+    for i in choices:
+        if formality in i["formality"]:
+            narrowed_choices.append(i)
+    return narrowed_choices
 
-print(pick())
+
+def main(formality):
+    choices = outfit["clothes"]
+    condition = warmth_factor(weather)
+    # Might as well combine these two
+    choices = apply_condition(choices, condition)
+    choices = apply_formality(choices, formality)
+    print(choices)
+
+
+main("casual")
 
 # Assumption that the user will input the formality
-
-
-
 
