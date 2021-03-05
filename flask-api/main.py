f6d7ca2b70ac264af89eb55fb8df3552cb006d48
Dax246 <damien.ambegoda@gmail.com>
2021-03-05 21:16:42 +1100

    continued to edit main.py

--- flask-api/main.py
+++ flask-api/main.py
@@ -2,7 +2,7 @@
 Main file that will contain all the python functions to be called by the front end
 
 """
-
+import json
 
 def outfit_selector(weather: str, formality:str):
     """
@@ -13,3 +13,13 @@ def outfit_selector(weather: str, formality:str):
         formality = string that says formality type (e.g. casual, smart-casual)
     :return: list of strings that is "colour type" of clothing picked in the outfit
     """
+
+    with open("test2.json") as json_file:
+        data = json.load(json_file)
+        # Example showing how to print all the type and colour from every item of clothing
+        for i in data["clothes"]:
+            print(i)
+
+
+
+outfit_selector("a", "b")
