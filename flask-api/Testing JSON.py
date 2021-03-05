d50ffcd8bd465d1df9ac4177fdf5607b2e065f86
Dax246 <damien.ambegoda@gmail.com>
2021-03-05 20:29:14 +1100

    fixed test2.json

--- flask-api/Testing JSON.py
+++ flask-api/Testing JSON.py
@@ -3,4 +3,13 @@ import json
 with open("test2.json") as json_file:
     data = json.load(json_file)
     print(json.dumps(data, indent=4))
-    print("yes")
+#    for i in range(5):
+#        print(data[i])
+
+with open("test.json") as json_file:
+    data = json.load(json_file)
+#    print(json.dumps(data, indent=4))
+#    print(data["firstName"])
+    for key in data:
+        print(key)
+        print(data[key])
