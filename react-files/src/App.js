f8b5bd07b9805b13c4eaba9e88190f4f1df0ce5d
ACC1100 <aaron.chao2000@gmail.com>
2021-03-05 22:19:24 +1100

    clean appjs add temp add clothes

--- react-files/src/App.js
+++ react-files/src/App.js
@@ -2,9 +2,21 @@ import React, { useState, useEffect } from 'react'; ////
 
 import {ChakraProvider, theme, Button, VStack, StackDivider, Box, Grid} from '@chakra-ui/react';
 
-import addClothes from "./add-clothes/addClothes";
+import AddClothes from "./add-clothes/addClothes";
 import MyClothes from "./my-clothes/myClothes";
 import Navbar from "./navbar/navbar";
+
+function getContent(page) {
+  if (page === "") {
+    return (
+        <AddClothes/>
+    )
+  } else if (page === "my-clothes") {
+    return (
+        <MyClothes/>
+    )
+  }
+}
 
 function App() {
   const [arg1, setArg1] = useState(5);
@@ -28,34 +40,12 @@ function App() {
   //     })
   // }
 
-
-  if (page === "") {
-    return (
-      <ChakraProvider theme={theme}>
-        <Navbar page={page} testf={setPage}/>
-
-          
-          <div>
-            {arg1}
-            <Button onClick={() => {setPage("my-clothes")}}>
-              Test
-            </Button>
-            {/* <addClothes/> */}
-            {addClothes()}
-          </div>
-
-      </ChakraProvider>
-    )
-  } else if (page === "my-clothes") {
-    return (
-      <ChakraProvider theme={theme}>
-        <Navbar page={page} testf={setPage}/>
-        <MyClothes/>
-      </ChakraProvider>
-        
-    )
-  }
-
+  return (
+    <ChakraProvider theme={theme}>
+      <Navbar page={page} testf={setPage}/>
+      {getContent(page)}
+    </ChakraProvider>
+  )
 }
 
 export default App;
--- react-files/src/add-clothes/addClothes.js
+++ react-files/src/add-clothes/addClothes.js
@@ -1,18 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'; ////
 import ReactDOM from 'react-dom';
 import {Text, ChakraProvider, theme, Button, VStack, StackDivider, Box, Grid} from '@chakra-ui/react';
 
-function clothesText(text) {
-    return <Text fontSize="md">{text}</Text>
-}
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+  } from "@chakra-ui/react"
 
-function addClothes() {
+import { useDisclosure } from "@chakra-ui/react"
+
+// function clothesText(text) {
+//     return <Text fontSize="md">{text}</Text>
+// }
+
+// function addClothes() {
+//     return (
+//         <div>
+//             {clothesText("hi")}
+//             {clothesText("asd")}
+//         </div>
+//     )
+// }
+
+function AddClothes() {
+    const [size, setSize] = React.useState("md")
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  
+    const handleClick = (newSize) => {
+      setSize(newSize)
+      onOpen()
+    }
+  
+    const sizes = ["xs", "sm", "md", "lg", "xl", "full"]
+  
     return (
-        <div>
-            {clothesText("hi")}
-            {clothesText("asd")}
-        </div>
+      <>
+        {sizes.map((size) => (
+          <Button
+            onClick={() => handleClick(size)}
+            key={size}
+            m={4}
+          >{`Open ${size} Drawer`}</Button>
+        ))}
+  
+        <Drawer onClose={onClose} isOpen={isOpen} size={size}>
+          <DrawerOverlay>
+            <DrawerContent>
+              <DrawerHeader>{`${size} drawer contents`}</DrawerHeader>
+              <DrawerBody>
+                {size === "full"
+                  ? `You're trapped 😆 , refresh the page to leave or press 'Esc' key.`
+                  : null}
+              </DrawerBody>
+            </DrawerContent>
+          </DrawerOverlay>
+        </Drawer>
+      </>
     )
-}
+  }
 
-export default addClothes;
+export default AddClothes;
