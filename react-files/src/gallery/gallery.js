f45828997100645175d49e7fd56be93d94d9d231
Allan C <allanchan960@gmail.com>
2021-03-06 21:24:50 +1100

    Parsing location

--- react-files/src/gallery/gallery.js
+++ react-files/src/gallery/gallery.js
@@ -8,69 +8,71 @@ import "./slick/slick-theme.css";
 
 
 function getGalleryItems() {
-    var output = [];
+  var output = [];
 
-    // const submitForm = () => {
-    //     var newData = formData;
-    //     newData['colour'] = ogColours[newData['colour']];
-    //     newData['type'] = types[newData['type']];
+  // const submitForm = () => {
+  //   var newData = formData;
+  //   newData['colour'] = ogColours[newData['colour']];
+  //   newData['type'] = types[newData['type']];
 
-    //     fetch('/add', {
-    //     method: "POST",
-    //     headers: {
-    //         "content_type": "application/json",
-    //     },
-    //     body: JSON.stringify(newData)
-    //     })
-    //     .then(response => response.json())
-    //     .then(data => {
-    //     console.log(data.result);
-    //     })
-    // }
-
-
+  //   fetch('/add', {
+  //   method: "POST",
+  //   headers: {
+  //       "content_type": "application/json",
+  //   },
+  //   body: JSON.stringify(newData)
+  //   })
+  //   .then(response => response.json())
+  //   .then(data => {
+  //   console.log(data.result);
+  //   })
+  // }
 }
 
 function Gallery() {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    };
+  const [location, setLocation] = useState('');
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 
-    const getMenu = () => {
-        return (
-            <Center>
-                <VStack spacing={8} w="80%">
-                    {/* <Input placeholder="location" size="lg" variant="filled" onChange={handleChange}/> */}
-                    <Button colorScheme="teal" size="lg" isFullWidth={true}>
-                        Generate
-                    </Button>
-                </VStack>
-            </Center>
-        )
-    }
-
+  const getMenu = () => {
     return (
-        <>
-            <Box p="2%">
-                {getMenu()}
-            </Box>
-
-            <>
-                <Center>
-                    <Box w="60%" borderRadius="20px">
-                        <Slider {...settings}>
-                            <Box w="20%" h="500px" bg="black" borderRadius="20px">asd</Box>
-                            <Box w="20%" h="500px" bg="black" borderRadius="20px">123</Box>
-                        </Slider>
-                    </Box>
-                </Center>
-            </>
-        </>
+      <Center>
+        <VStack spacing={8} w="80%">
+          <Input placeholder="location" size="lg" variant="filled" onChange={(event) => {
+            setLocation(event.target.value);
+            console.log(event.target.value);
+          }} />
+          <Button colorScheme="teal" size="lg" isFullWidth={true}>
+              Generate
+          </Button>
+        </VStack>
+      </Center>
     )
+  }
+
+  return (
+    <>
+      <Box p="2%">
+        {getMenu()}
+      </Box>
+
+      <>
+        <Center>
+          <Box w="60%" borderRadius="20px">
+            <Slider {...settings}>
+              <Box w="20%" h="500px" bg="black" borderRadius="20px">asd</Box>
+              <Box w="20%" h="500px" bg="black" borderRadius="20px">123</Box>
+            </Slider>
+          </Box>
+        </Center>
+      </>
+    </>
+  )
 }
 
 export default Gallery;
