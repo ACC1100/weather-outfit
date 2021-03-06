810b92a4d802f09e3f25e03cae22fc0d65f57838
Allan C <allanchan960@gmail.com>
2021-03-06 18:40:46 +1100

    Added placeholder images

--- react-files/src/my-clothes/clothingElement.js
+++ react-files/src/my-clothes/clothingElement.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Text, Box, Image, Center } from '@chakra-ui/react';
-import top from './top.png';
-import middlewear from './middlewear.png';
-import deleteSymbol from './delete.png';
+import top from './pictures/top.png';
+import middlewear from './pictures/middlewear.png';
+import outerwear from './pictures/outerwear.png';
+import bottom from './pictures/bottom.png';
+import footwear from './pictures/shoe.png';
+import headwear from './pictures/headwear.png';
+import deleteSymbol from './pictures/delete.png';
 import './myClothes.css';
 
 const ClothingElement = ({ id, index_i, index_j }) => {
-  const pictures = [];
+  const pictures = [top, middlewear, outerwear, bottom, footwear, headwear];
   return (
     <Box className='imageGrow' id={id} borderWidth='1px' height='auto' borderRadius='20px' m='20px' textAlign='center'>
       <Center>
-        <Image className='imagex' index_i={index_i} index_j={index_j} borderRadius='20px'
-          objectFit='contain' src={top} alt='' />
+        <Image className='imagex' index_i={index_i} index_j={index_j} borderRadius='20px'  pt='10px'
+          objectFit='contain' boxSize='150px' src={pictures[index_i]} alt='' />
       </Center>
       <br/>
       <Text>Short-sleeve T-shirt</Text>
